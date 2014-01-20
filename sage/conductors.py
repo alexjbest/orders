a03a9a210647ec3f4202f3bc06a63f2a364ed3fe
Alex J Best <alex.j.best@gmail.com>
2014-01-20 22:05:12 +0000

    conductors seem to work now

--- sage/conductors.py
+++ sage/conductors.py
@@ -5,19 +5,23 @@ def conductor(order):
     K = order.fraction_field()
     R = Integers()
     omega = K.maximal_order().basis()
-    tau = order.basis()
     n = len(omega)
-    b = [[0]*n for i in range(n)]
     M = matrix(R.fraction_field(), nrows = n*n, ncols = n)
 
+    d = 1
     for i in range(n):
         for j in range(n):
             coords = order.coordinates(omega[i]*omega[j])
             for k in range(n):
                 M[j*n + k, i] = coords[k]
+                d = lcm(d,coords[k].denominator())
     print M
-        
-    #d = gcd()
-    #H = (d*M).hermite_form()
-    #beta = omega*d*H.inverse()
-    #return K.ideal(beta)
+    print d
+    
+    H = (M).hermite_form(include_zero_rows = False)
+    print H
+    print d*H.inverse()
+    
+    beta = vector(omega)*d*H.inverse()
+    print beta
+    return K.ideal(list(beta))
