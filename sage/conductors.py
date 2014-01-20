a06511f249c6951eb7652b207e5701e55a41b89c
Alex J Best <alex.j.best@gmail.com>
2014-01-20 20:04:31 +0000

    Sorting out matrices

--- sage/conductors.py
+++ sage/conductors.py
@@ -2,14 +2,21 @@ def conductor(order):
     """
     Return the conductor of the order.
     """
-    print "hi"
     K = order.fraction_field()
+    R = Integers()
     omega = K.maximal_order().basis()
     tau = order.basis()
     n = len(omega)
+    b = [[0]*n for i in range(n)]
+    M = matrix(R, nrows = n*n, ncols = n)
+
     for i in range(n):
         for j in range(n):
-            print order.coordinates(omega[i]*omega[j])
+            coords = order.coordinates(omega[i]*omega[j])
+            for k in range(n):
+                M[j*n + k, i] = coords[k]
+    print M
+        
     #d = gcd()
     #H = (d*M).hermite_form()
     #beta = omega*d*H.inverse()
