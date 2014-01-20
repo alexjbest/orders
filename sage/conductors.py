875c7a4589f405f0ec3447ba5c680d6ba8cfef34
Alex J Best <alex.j.best@gmail.com>
2014-01-20 19:15:18 +0000

    working out coords

--- sage/conductors.py
+++ sage/conductors.py
@@ -3,4 +3,14 @@ def conductor(order):
     Return the conductor of the order.
     """
     print "hi"
-    pass
+    K = order.fraction_field()
+    omega = K.maximal_order().basis()
+    tau = order.basis()
+    n = len(omega)
+    for i in range(n):
+        for j in range(n):
+            print coordinates(omega[i]*omega[j])
+    #d = gcd()
+    #H = (d*M).hermite_form()
+    #beta = omega*d*H.inverse()
+    #return K.ideal(beta)
