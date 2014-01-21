33e4e746d1d968950f7d20cd95d066e5c293f3a2
Alex J Best <alex.j.best@gmail.com>
2014-01-21 14:28:44 +0000

    Got basic code working in sage a bit

--- sage/conductors.py
+++ sage/conductors.py
@@ -25,3 +25,59 @@ def conductor(order):
     beta = vector(omega)*d*H.inverse()
     print beta
     return K.ideal(list(beta))
+
+def order_of_index(O,I):
+    primes = dict() # primes[p][i] will contain all prime ideals with norm p^i.
+    K = O.fraction_field()
+    ZK = K.maximal_order()
+    R = Integers() # This may need to be different when relative orders are looked for.
+
+    for (p,e) in (I*I).factor():
+        #print str(p)+"^"+str(e)
+        primes[p] = dict()
+        for i in range(1,e+1):
+            primes[p][i] = []
+
+        for (P,E) in K.ideal(p).factor():
+            v = valuation(P.norm(), p)
+            if v in primes[p]:
+                primes[p][v].append(P)
+
+    #print primes
+    # We use the factorisation of prime ideals to find all ideals of norm I^2, which are all possibly conductors of O
+    possible_conductors = [K.ideal(1)]
+    for (p,e) in (I*I).factor():
+        possible_factorisations = []
+        for partition in Partitions(e, parts_in = primes[p].keys()):
+            #print partition
+            current_factorisation = [K.ideal(1)]
+            for i in partition:
+                new_factorisation = []
+                for P in primes[p][i]:
+                    for f in current_factorisation:
+                        new_factorisation.append(f*P)
+                current_factorisation = new_factorisation
+            possible_factorisations += new_factorisation
+
+        current_conductors = []
+        for P in possible_factorisations:
+            for f in possible_conductors:
+                current_conductors.append(f*P)
+        possible_conductors = current_conductors
+        #print possible_conductors
+
+    orders = []
+    for f in possible_conductors:
+        #print f
+        #print f.basis()
+        Of = K.order(f.basis()) # Use ring_generators here?!
+        if conductor(Of) == f:
+            if Of.index_in(ZK) == I:
+                orders.append(Of)
+            else:
+                raise Exception(str(Of) + " has index " + str(Of.index_in(ZK)) + " but " + str(f))
+    
+    return orders
+            
+
+
